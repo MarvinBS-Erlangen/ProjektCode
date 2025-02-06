41168eac5d2a4791182f59cb01c67847d001ebde
Mateyw <mateusz.mazik@adidas.com>
2025-02-06 11:29:15 +0100

    Added styles to register.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <ul class="footer-list">
+        <li><a href="/impressum">Impressum</a></li>
+        <li><a href="/agbs">AGBs</a></li>
+        <li><a href="/datenschutzerklaerung">Datenschutzerklärung</a></li>
+        <li><a href="/kontakt">Kontakt</a></li>
+    </ul>
+</footer>
--- login.php
+++ login.php
@@ -54,24 +54,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <body>
 
-    <header style="height: clamp(3rem, 10vw, 5rem);" class="header">
-        <div class="header-title">
-            <span id="title" class="title"><a href="/">MACaPPLE</a></span>
-        </div>
-        <div class="nav-links">
-            <ul class="nav-list">
-                <li class="btn-menus"><a href="/menus">Menus</a></li>
-                <li class="btn-products"><a href="/products">Products</a></li>
-                <li class="btn-contest"><a href="/contest">Contest</a></li>
-                <li class="btn-login"><a href="/login">Login</a></li>
-                <li class="btn-signup"><a href="/signup">Sign Up</a></li>
-            </ul>
-        </div>
-        <div class="cart-container">
-            <i class="fa-solid fa-cart-shopping cart-icon"></i>
-            <span class="items-in-cart">0</span>
-        </div>
-    </header>
+      <?php include 'header.php'; ?>
+
 
 
     <div class="form-container">
@@ -89,14 +73,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <p class="not-registered">Noch kein Konto? <a href="register.php">Registrieren</a></p>
     </div>
 
-    <footer>
-        <ul class="footer-list">
-            <li><a href="/impressum">Impressum</a></li>
-            <li><a href="/agbs">AGBs</a></li>
-            <li><a href="/datenschutzerklaerung">Datenschutzerklärung</a></li>
-            <li><a href="/kontakt">Kontakt</a></li>
-        </ul>
-    </footer>
+    <?php include 'footer.php'; ?>
+
 </body>
 
 </html>
