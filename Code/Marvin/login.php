ffe3585b94d9be57d1247826eab8bb79f9c1c72f
SpaghettiCodeExperte <marvin.inkermann@bs-erlangen.de>
2025-02-06 09:00:07 +0100

    Revert "Added styles to login.php; Added login.css file"
    This reverts commit cbea28412290a6d3a539a24682a0074b50475fe0.

--- Code/Marvin/login.php
+++ Code/Marvin/login.php
@@ -1,22 +1,25 @@
 <?php
 session_start();
-// Verbindung zur Datenbank herstellen
+//Verbindung zur Datenbank herstellen 
 include 'connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $emailKunde = $_POST['EMail'];
     $passwordKunde = $_POST['Password'];
 
-    // Prepared Statement zur Vermeidung von SQL Injections
+    // Benutzungung von prst -> Prepared statement um SQL Injections zu verhindern
     $prst = $conn->prepare("SELECT Password_Hash FROM kunde WHERE EMail = ?");
+    // Der Platzhalter ? wird durch den string wert "s" im emailKunde ersetzt
     $prst->bind_param("s", $emailKunde);
+    // ausfuehren des prst
     $prst->execute();
+    // Ergebnis speichern
     $prst->store_result();
 
     if ($prst->num_rows > 0) {
         $prst->bind_result($hashedPassword);
         $prst->fetch();
-
+        
         if (password_verify($passwordKunde, $hashedPassword)) {
             echo "<p class='success'>Login successful!</p>";
         } else {
@@ -32,27 +35,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 
 <!DOCTYPE html>
-<html lang="de">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="./styles/login.css">
 </head>
 <body>
-    <div class="form-container">
-        <h2 class="login-title">Login</h2>
-        <form action="login.php" method="post">
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="EMail" required>
-
-            <label for="password">Passwort:</label>
-            <input type="password" id="password" name="Password" required>
-
-            <input type="submit" id="btn-login" value="Login">
-        </form>
-
-        <p class="not-registered">Noch kein Konto? <a href="register.php">Registrieren</a></p>
-    </div>
+    <h2>Login</h2>
+    <form action="login.php" method="post">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="EMail" required><br><br>
+        
+        <label for="password">Passwort:</label>
+        <input type="password" id="password" name="Password" required><br><br>
+        
+        <input type="submit" value="Login">
+    </form>
 </body>
 </html>
