b4d4ce81d74c3c1a86941256c394b8412131f39c
Ogner, Ioana <>
2025-03-18 08:42:36 +0100

    test

--- src/views/bestellungcomplete.php
+++ src/views/bestellungcomplete.php
@@ -5,7 +5,6 @@ include '../comps/usercheck.php';
 
 <!DOCTYPE html>
 <html lang="de">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,7 +16,7 @@ include '../comps/usercheck.php';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Bestellung Abgeschlossen</title>
+    <title>Bestellung Abgeschlossen!</title>
 </head>
 
 <body>
