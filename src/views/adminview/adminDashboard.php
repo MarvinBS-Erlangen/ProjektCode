369f9724d915d54d37ba6fdcdd95cdbef3a712e0
SpaghettiCodeExperte <marvin.inkermann@bs-erlangen.de>
2025-03-18 09:39:51 +0100

    added link to dashboard

--- src/views/adminview/adminDashboard.php
+++ src/views/adminview/adminDashboard.php
@@ -34,6 +34,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'logout') {
         <li><a href="ZutatAdmin.php">Zutat Admin</a></li>
         <li><a href="ProduktZutatAdmin.php">Produkt_Zutat</a></li>
         <li><a href="bestellungAdmin.php">Bestellungen</a></li>
+        <li><a href="csvDownload.php">DatenCSV</a></li>
         <li><a href="adminDashboard.php?action=logout">Abmelden</a></li>
     </ul>
 </body>
