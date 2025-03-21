d12bb6cc26b89d8db58b200e17dc695a1f2f19eb
Mateyw <mateusz.mazik@adidas.com>
2025-03-21 12:01:36 +0100

    add regex contact

--- src/views/contact.php
+++ src/views/contact.php
@@ -10,6 +10,7 @@
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
         <script src="../handlers/btn-back-handler.js" defer></script>
+        <script src="../handlers/validation_contact.js" defer></script>
         <title>Kontakt – MACaPPLE</title>
 </head>
 
